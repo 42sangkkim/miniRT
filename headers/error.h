0ce856a911ce5ee8ed75715a63b0ca3b70ee367a
sangkkim <madlogic365@gmail.com>
2023-01-04 12:24:44 +0900

    add headers

---
+++ headers/error.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangkkim <sangkkim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/02 17:50:06 by sangkkim          #+#    #+#             */
+/*   Updated: 2023/01/02 17:51:34 by sangkkim         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_H
+# define ERROR_H
+
+# define INVALID_ARGUMENT				1
+# define INVALID_FILE					2
+# define FILE_ERROR						3
+# define MALLOC_ERROR					4
+
+#endif//ERROR_H
---
+++ headers/point.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   point.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangkkim <sangkkim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/22 01:45:22 by sangkkim          #+#    #+#             */
+/*   Updated: 2022/12/22 01:46:42 by sangkkim         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef POINT_H
+# define POINT_H
+
+typedef struct s_point3			t_point3;
+
+struct s_point3
+{
+	double			x;
+	double			y;
+	double			z;
+};
+
+#endif//POINT_H
